64f5e5dd8febf04fce60c4cf0948418226af5342
Waldemar Bira <waldek.bira@gmail.com>
2016-01-13 20:06:34 +0100

    Added map on contect section, that displaying Unislaw on google maps

--- js/app.js
+++ js/app.js
@@ -3,12 +3,36 @@ $(document).ready(function() {
 
     (function UIModule() {
         function initialize() {
+        	//todo: refactoring
             $('#nav').affix({
                 offset: {
                     top: $("header").outerHeight(true) - 50
                 }
             });
+
+            //apply google listener
+            google.maps.event.addDomListener(window, 'load', mapInitializer);
         }
+
+        function mapInitializer() {
+            var mapCanvas = document.getElementById('map'),
+            	coords = new google.maps.LatLng(50.7135,16.2406),
+            	mapOptions = {
+            		center: coords,
+            		zoom: 11,
+            		mapTypeId: google.maps.MapTypeId.ROADMAP,
+            	},
+            	markerOptions = {
+            		position: coords,
+            		title: 'Stajnia u Kalinów'
+            	}
+                map;
+
+            map = new google.maps.Map(mapCanvas, mapOptions);
+            markerOptions.map = map;
+            new google.maps.Marker(markerOptions);
+        }
+
         //public API
         return {
             init: initialize
@@ -17,4 +41,3 @@ $(document).ready(function() {
     }()).init();
 
 });
-
